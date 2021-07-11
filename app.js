f1c0260db9598a758c51ec06b0ceef77c2dbf7c1
Andres <wellmann39@gmail.com>
2021-07-11 12:25:51 -0600

    Subida del proyecto

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const hbs = require('hbs');
 require('dotenv').config();
 
 const app  = express();
 const port = process.env.PORT; 
-
-// Handlebars
-app.set('view engine', 'hbs');
-hbs.registerPartials( __dirname + '/views/partials');
 
 // Servir contenido estatico
 app.use( express.static('public') );
