ec9784a568d418bcdb4977a4d8e31a08b759820c
Andres <wellmann39@gmail.com>
2021-07-11 12:55:24 -0600

    Portafolio con server

---
+++ public/precache-manifest.2461372eadd01aeefd1d47c93c9f569a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347f89c805ec0401c3223080b3bbb5de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be0a61eca6629ba1bc4",
+    "url": "/static/css/main.e23565fb.chunk.css"
+  },
+  {
+    "revision": "347af89a343dda046580",
+    "url": "/static/js/2.a12c21e9.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/static/js/2.a12c21e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be0a61eca6629ba1bc4",
+    "url": "/static/js/main.f95c8f92.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
