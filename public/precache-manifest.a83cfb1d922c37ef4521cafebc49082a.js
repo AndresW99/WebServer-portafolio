1b6807e9c572570ca77503e406890ed47eca383e
Andres <wellmann39@gmail.com>
2021-07-11 18:48:59 -0600

    API de rick and morty agregada

--- public/precache-manifest.a83cfb1d922c37ef4521cafebc49082a.js
+++ public/precache-manifest.a83cfb1d922c37ef4521cafebc49082a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "347f89c805ec0401c3223080b3bbb5de",
+    "revision": "29461d8e121ee2a2e9b05d83408bdb55",
     "url": "/index.html"
   },
   {
-    "revision": "1be0a61eca6629ba1bc4",
+    "revision": "70172d5dfa544ed6b506",
     "url": "/static/css/main.e23565fb.chunk.css"
   },
   {
-    "revision": "347af89a343dda046580",
-    "url": "/static/js/2.a12c21e9.chunk.js"
+    "revision": "ebabec390e50be9606ba",
+    "url": "/static/js/2.adb0e8be.chunk.js"
   },
   {
     "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
-    "url": "/static/js/2.a12c21e9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.adb0e8be.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1be0a61eca6629ba1bc4",
-    "url": "/static/js/main.f95c8f92.chunk.js"
+    "revision": "70172d5dfa544ed6b506",
+    "url": "/static/js/main.016886d1.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2461372eadd01aeefd1d47c93c9f569a.js"
+  "/precache-manifest.a83cfb1d922c37ef4521cafebc49082a.js"
 );
 
 self.addEventListener('message', (event) => {
